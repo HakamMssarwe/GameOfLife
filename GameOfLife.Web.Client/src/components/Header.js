d980da504c808a73b1de7ff12b58f16bcab7fb40
Hakam <HakamStudent@gmail.com>
2022-03-10 16:51:57 +0200

    small changes

--- GameOfLife.Web.Client/src/components/Header.js
+++ GameOfLife.Web.Client/src/components/Header.js
@@ -15,10 +15,10 @@ export default function Header({gameIsRunning, initiateGame, stopGame, boardSize
 							<NavDropdown.Item onClick={e => setBoardSize({name: "10x10", value: 10})}>10x10</NavDropdown.Item>
 							<NavDropdown.Item onClick={e => setBoardSize({name: "15x15", value: 15})}>15x15</NavDropdown.Item>
 							<NavDropdown.Item onClick={e => setBoardSize({name: "20x20", value: 20})}>20x20</NavDropdown.Item>
-							<NavDropdown.Item onClick={e => setBoardSize({name: "30x30", value: 30})}>30x30</NavDropdown.Item>
+							{/* <NavDropdown.Item onClick={e => setBoardSize({name: "30x30", value: 30})}>30x30</NavDropdown.Item>
 							<NavDropdown.Item onClick={e => setBoardSize({name: "50x50", value: 50})}>50x50</NavDropdown.Item>
 							<NavDropdown.Item onClick={e => setBoardSize({name: "75x75", value: 75})}>75x75</NavDropdown.Item>
-							<NavDropdown.Item onClick={e => setBoardSize({name: "100x100", value: 100})}>100x100</NavDropdown.Item>
+							<NavDropdown.Item onClick={e => setBoardSize({name: "100x100", value: 100})}>100x100</NavDropdown.Item> */}
 						</NavDropdown>
 						<NavDropdown title={population.name} id="population">
 							<NavDropdown.Item onClick={e => setPopulation({name: "Small", value: 0})}>Small</NavDropdown.Item>
